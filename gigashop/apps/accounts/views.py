4867c1fd4dc0e8961206f3e545fdbb969b5240ee
iliaman <iliaman1993@gmail.com>
2023-12-18 14:12:47 +0300

    view to create a user

--- gigashop/apps/accounts/views.py
+++ gigashop/apps/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from .serializers import UserSerializer
+from .tasks import confirmation_email
+
+
+class UserCreateView(CreateAPIView):
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+
+        confirmation_email.send(user.pk)
